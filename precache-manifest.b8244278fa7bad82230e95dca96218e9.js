8619d56184ae45f735d265869e4e4e54a222bc12
Junaid Ul Haque <junaidulhaque22@gmail.com>
2020-07-03 20:52:13 +0500

    Updates

---
+++ precache-manifest.b8244278fa7bad82230e95dca96218e9.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d5e3d49d351eeed0423acbe4f52ee002",
+    "url": "/battleship-game/index.html"
+  },
+  {
+    "revision": "3a576c48d767daaec63d",
+    "url": "/battleship-game/static/css/main.cf72b78f.chunk.css"
+  },
+  {
+    "revision": "4739b66f57f6f41f979c",
+    "url": "/battleship-game/static/js/2.cf81fd4e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/battleship-game/static/js/2.cf81fd4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a576c48d767daaec63d",
+    "url": "/battleship-game/static/js/main.c68d26ea.chunk.js"
+  },
+  {
+    "revision": "e373f843834f2065828f",
+    "url": "/battleship-game/static/js/runtime-main.71a699a1.js"
+  },
+  {
+    "revision": "c1427546d3bdcf9b89751b110ee7e544",
+    "url": "/battleship-game/static/media/battleship.c1427546.png"
+  },
+  {
+    "revision": "ce59afd41fb167f23ae964d926b0e7c8",
+    "url": "/battleship-game/static/media/done-24px.ce59afd4.svg"
+  },
+  {
+    "revision": "3736a9049c0b8617f5fad46b9d7733cc",
+    "url": "/battleship-game/static/media/invertedBattleship.3736a904.png"
+  },
+  {
+    "revision": "aae49de0f425e09eb78593100977a1af",
+    "url": "/battleship-game/static/media/refresh-24px.aae49de0.svg"
+  },
+  {
+    "revision": "7d953db9a9510a12f6ea0657304859f2",
+    "url": "/battleship-game/static/media/undo-24px.7d953db9.svg"
+  }
+]);
